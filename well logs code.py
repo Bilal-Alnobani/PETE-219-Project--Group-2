3eb05eb91edfbbbd3cb6714a7aecf93bb6cca187
Mohammed-Al-Hadad <151367898+Mohammed-Al-Hadad@users.noreply.github.com>
2023-12-08 00:57:31 +0300

    Update well logs code.py
    adding histograms to view the distributions of the different datasets that include the mean, 5th percentile, 95 percentile and median for the GR, CNPOR, RHOB, RILD

--- well logs code.py
+++ well logs code.py
@@ -66,3 +66,74 @@ plt.figure(figsize=(16, 6))
 heatmap = sns.heatmap(well_df.corr(), vmin=-1, vmax=1, annot=True)
 heatmap.set_title('Log Properties Correlation Heatmap', fontdict={'fontsize':12}, pad=12);
 plt.show()
+
+#histograms 
+### Plotting Histograms ###
+## RHOB histogram
+plt.hist(well_df['RHOB'], bins=20, color='red', alpha=0.5, edgecolor='black')
+plt.ylabel("frequency", fontsize=14)
+plt.xlabel("RHOB", fontsize= 14)
+plt.xlim(0,4)
+
+mean_RHOB = well_df['RHOB'].mean()
+p5_RHOB= well_df['RHOB'].quantile(0.05)
+p95_RHOB = well_df['RHOB'].quantile(0.95)
+median_RHOB = well_df['RHOB'].median()
+plt.axvline(mean_RHOB,color='blue', label='mean')
+plt.axvline(p5_RHOB,color='green', label='5th Percentile')
+plt.axvline(p95_RHOB,color='purple', label='95th Percentile')
+plt.axvline(median_RHOB,color='red', label='Median')
+plt.legend()
+plt.show()
+
+## GR histogram
+plt.hist(well_df['GR'], bins=50, color='blue', alpha=0.5, edgecolor='black')
+plt.ylabel("frequency", fontsize=14)
+plt.xlabel("GR", fontsize= 14)
+plt.xlim(0,200)
+
+mean_GR = well_df['GR'].mean()
+p5_GR= well_df['GR'].quantile(0.05)
+p95_GR = well_df['GR'].quantile(0.95)
+median_GR = well_df['GR'].median()
+plt.axvline(mean_GR,color='blue', label='mean')
+plt.axvline(p5_GR,color='green', label='5th Percentile')
+plt.axvline(p95_GR,color='purple', label='95th Percentile')
+plt.axvline(median_GR,color='red', label='Median')
+plt.legend()
+plt.show()
+
+## CNPOR Histogram
+plt.hist(well_df['CNPOR'], bins=70, color='green', alpha=0.5, edgecolor='black')
+plt.ylabel("frequency", fontsize=14)
+plt.xlabel("CNPOR", fontsize= 14)
+plt.xlim(0,80)
+
+mean_CNPOR = well_df['CNPOR'].mean()
+p5_CNPOR= well_df['CNPOR'].quantile(0.05)
+p95_CNPOR = well_df['CNPOR'].quantile(0.95)
+median_CNPOR = well_df['CNPOR'].median()
+plt.axvline(mean_CNPOR,color='blue', label='mean')
+plt.axvline(p5_CNPOR,color='black', label='5th Percentile')
+plt.axvline(p95_CNPOR,color='purple', label='95th Percentile')
+plt.axvline(median_CNPOR,color='red', label='Median')
+plt.legend()
+plt.show()
+
+## RILD Histogram
+plt.hist(well_df['RILD'], bins=70, color='green', alpha=0.5, edgecolor='black')
+plt.ylabel("frequency", fontsize=14)
+plt.xlabel("RILD", fontsize= 14)
+plt.xlim(0,100)
+
+mean_RILD = well_df['RILD'].mean()
+p5_RILD= well_df['RILD'].quantile(0.05)
+p95_RILD = well_df['RILD'].quantile(0.95)
+median_RILD = well_df['RILD'].median()
+plt.axvline(mean_RILD,color='blue', label='mean')
+plt.axvline(p5_RILD,color='black', label='5th Percentile')
+plt.axvline(p95_RILD,color='purple', label='95th Percentile')
+plt.axvline(median_RILD,color='red', label='Median')
+plt.legend()
+plt.show()
+
