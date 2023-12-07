109d3edf19ca668a06a19130c6f846a7719ed7ea
Mohammed-Al-Hadad <151367898+Mohammed-Al-Hadad@users.noreply.github.com>
2023-12-08 01:03:17 +0300

    Update Image code.py
    image analysis of the cross-section image of the reservoir with calculated porosity and including histograms to convert the image to a property vector with binary image thresholding

--- Image code.py
+++ Image code.py
@@ -1,3 +1,39 @@
 ##### Image Analysis code goes here: #####
 
+#### importing libraries
+from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
+#### Viewing image
+image = Image.open('berea8bit.tif')
+image.show()
+imarray = np.array(image)
+
+img = io.imread('berea8bit.tif')
+plt.imshow(img,cmap='gray')
+plt.axis('off')
+plt.show()
+imarray = np.array(img)
+
+#### displaying image as a histogram
+# convert image to a property vector
+imV = imarray.reshape((500*500, 1))
+# histogram
+plt.hist(imV, density=True, bins=30, range=[120,270]) 
+plt.show()
+
+### binary image thresholding
+BW = imarray
+BW[BW<100] = 0
+BW[BW>=100] = 255
+BW2 = BW.astype(bool)
+BW2 = np.array(BW2)
+plt.imshow(imarray, cmap='Greys_r')
+plt.axis('off')
+
+area = np.size(BW2)
+fw = np.sum(BW2)
+
+print('The porosity of the thin section is:', (1 - (fw/area))*100)
